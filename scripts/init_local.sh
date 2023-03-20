e9a561499579cf622f627344adba7abf661f450a
willstahl <will@thedenom.org>
2023-03-20 10:59:12 -0700

    Fixed sed in-place commands so they work on mac

--- scripts/init_local.sh
+++ scripts/init_local.sh
@@ -42,21 +42,24 @@ noriad gentx me 100000000$DENOM --chain-id oasis-3 --commission-rate 0.1 --commi
 noriad collect-gentxs > /dev/null 2>&1
 
 # Edit genesis
-sed -i "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+rm $CONFIG_HOME/genesis.json.bak
 
 # Edit config.toml to set the block speed to 1s
-sed -i 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' $CONFIG_HOME/config.toml > /dev/null 2>&1
+sed -i.bak 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' $CONFIG_HOME/config.toml > /dev/null 2>&1
+rm $CONFIG_HOME/config.toml.bak
 
 # Edit app.toml to set the minimum gas price
-sed -i "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$GAS_PRICE_DENOM\"/g" $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i.bak "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$GAS_PRICE_DENOM\"/g" $CONFIG_HOME/app.toml > /dev/null 2>&1
 
 # Edit app.toml to enable LCD REST server on port 1317 and REST documentation at http://localhost:1317/swagger/
-sed -i 's/^enable\ =\ false/enable\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
-sed -i 's/^swagger\ =\ false/swagger\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i.bak 's/^enable\ =\ false/enable\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i.bak 's/^swagger\ =\ false/swagger\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
+rm $CONFIG_HOME/app.toml.bak
 
 echo -e "\n\nYou can now start your chain with 'noriad start'\n"
