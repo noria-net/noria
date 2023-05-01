1430136dd1ee36fde08354d62db91bd0b966991b
Mat <bourgeois.mat@gmail.com>
2023-05-01 14:56:47 -0700

    working 47.1, naked

--- app/app.go
+++ app/app.go
@@ -128,12 +128,12 @@ import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 )
 
-const appName = "WasmApp"
+const appName = "Noria"
 
 // We pull these out so we can set them with LDFLAGS in the Makefile
 var (
-	NodeDir      = ".wasmd"
-	Bech32Prefix = "wasm"
+	NodeDir      = ".noria"
+	Bech32Prefix = "noria"
 
 	// If EnabledSpecificProposals is "", and this is "true", then enable all x/wasm proposals.
 	// If EnabledSpecificProposals is "", and this is not "true", then disable all x/wasm proposals.
--- scripts/init_local.sh
+++ scripts/init_local.sh
@@ -30,36 +30,36 @@ noriad config chain-id $CHAIN_ID
 noriad keys add me
 
 # Initialize a new genesis.json file
-noriad init me --chain-id $CHAIN_ID > /dev/null 2>&1 
+noriad init me --overwrite --chain-id $CHAIN_ID
 
 # Add your freshly created account to the new chain genesis
-noriad add-genesis-account me 1000000000$GAS_PRICE_DENOM,1000000000$DENOM > /dev/null 2>&1 
+noriad genesis add-genesis-account me 1000000000$GAS_PRICE_DENOM,1000000000$DENOM
 
 # Generate the genesis transaction to create a new validator
-noriad gentx me 100000000$DENOM --chain-id oasis-3 --commission-rate 0.1 --commission-max-rate 0.2 --commission-max-change-rate 0.01 > /dev/null 2>&1
+noriad genesis gentx me 100000000$DENOM --chain-id oasis-3 --commission-rate 0.1 --commission-max-rate 0.2 --commission-max-change-rate 0.01
 
 # Add that gentx transaction to the genesis file
-noriad collect-gentxs > /dev/null 2>&1
+noriad genesis collect-gentxs
 
 # Edit genesis
-sed -i.bak "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i.bak 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i.bak 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i.bak 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i.bak 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
-sed -i.bak 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json > /dev/null 2>&1
+sed -i.bak "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json
+sed -i.bak 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json
+sed -i.bak 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json
+sed -i.bak 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json
+sed -i.bak 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json
+sed -i.bak 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json
 rm $CONFIG_HOME/genesis.json.bak
 
 # Edit config.toml to set the block speed to 1s
-sed -i.bak 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' $CONFIG_HOME/config.toml > /dev/null 2>&1
+sed -i.bak 's/^timeout_commit\ =\ .*/timeout_commit\ =\ \"1s\"/g' $CONFIG_HOME/config.toml
 rm $CONFIG_HOME/config.toml.bak
 
 # Edit app.toml to set the minimum gas price
-sed -i.bak "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$GAS_PRICE_DENOM\"/g" $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i.bak "s/^minimum-gas-prices\ =\ .*/minimum-gas-prices\ =\ \"0.0025$GAS_PRICE_DENOM\"/g" $CONFIG_HOME/app.toml
 
 # Edit app.toml to enable LCD REST server on port 1317 and REST documentation at http://localhost:1317/swagger/
-sed -i.bak 's/^enable\ =\ false/enable\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
-sed -i.bak 's/^swagger\ =\ false/swagger\ =\ true/g' $CONFIG_HOME/app.toml > /dev/null 2>&1
+sed -i.bak 's/^enable\ =\ false/enable\ =\ true/g' $CONFIG_HOME/app.toml
+sed -i.bak 's/^swagger\ =\ false/swagger\ =\ true/g' $CONFIG_HOME/app.toml
 rm $CONFIG_HOME/app.toml.bak
 
 echo -e "\n\nYou can now start your chain with 'noriad start'\n"
