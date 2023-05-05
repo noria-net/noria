3a3235b1968b14f215ee8247b94b7fca268cc7b8
Mat <bourgeois.mat@gmail.com>
2023-05-05 13:10:40 -0700

    updated param change script for sdk47 compatibility, shortened voting period in local env

--- scripts/init_local.sh
+++ scripts/init_local.sh
@@ -46,7 +46,7 @@ sed -i.bak "s/stake/$DENOM/g" $CONFIG_HOME/genesis.json
 sed -i.bak 's/"inflation": "[^"]*"/"inflation": "0\.0"/g' $CONFIG_HOME/genesis.json
 sed -i.bak 's/"inflation_rate_change": "[^"]*"/"inflation_rate_change": "0\.0"/g' $CONFIG_HOME/genesis.json
 sed -i.bak 's/"inflation_min": "[^"]*"/"inflation_min": "0\.0"/g' $CONFIG_HOME/genesis.json
-sed -i.bak 's/"voting_period": "[^"]*"/"voting_period": "60s"/g' $CONFIG_HOME/genesis.json
+sed -i.bak 's/"voting_period": "[^"]*"/"voting_period": "10s"/g' $CONFIG_HOME/genesis.json
 sed -i.bak 's/"quorum": "[^"]*"/"quorum": "0.000001"/g' $CONFIG_HOME/genesis.json
 rm $CONFIG_HOME/genesis.json.bak
 
---
+++ scripts/param_change_sdk47.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PROPOSAL_FILE=$1
+KEY_NAME=$2
+BINARY_DIR=".noria"
+CHAIN_ID="oasis-3"
+DENOM="unoria"
+GAS_PRICE_DENOM="ucrd"
+GAS_PRICE="0.0025"
+NODE="http://127.0.0.1:26657/"
+export DAEMON_NAME="noriad"
+export DAEMON_HOME="$HOME/$BINARY_DIR"
+
+# Block height and sender key name must be set
+if [ -z "$1" ] && [ -z "$2" ]; then
+  echo "Parameter file and/or sender key name is missing"
+  exit 1
+fi
+
+exe() { echo "EXE\$ $@" ; "$@" ; }
+
+# submit parameter change proposal
+exe $DAEMON_NAME tx gov submit-legacy-proposal param-change $PROPOSAL_FILE \
+  --from $KEY_NAME \
+  --chain-id $CHAIN_ID \
+  --home $DAEMON_HOME \
+  --node $NODE \
+  --yes \
+  --gas-prices $GAS_PRICE$GAS_PRICE_DENOM \
+  --gas auto \
+  --gas-adjustment 1.75 \
+
+sleep 2
+
+PROPOSAL_ID=$($DAEMON_NAME q gov proposals limit 1 --reverse --output json --home $DAEMON_HOME --node $NODE | jq '.proposals[0].id | tonumber')
+
+# vote on the proposal
+exe $DAEMON_NAME tx gov vote $PROPOSAL_ID yes --from $KEY_NAME --chain-id $CHAIN_ID --home $DAEMON_HOME --node $NODE --yes --gas-prices $GAS_PRICE$GAS_PRICE_DENOM --gas auto --gas-adjustment 1.75
